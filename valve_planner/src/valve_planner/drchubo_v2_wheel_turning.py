96f293498be45f655ff56f6d0d21c7e555bf12c9
Jim Mainprice <mainprice@gmail.com>
2013-10-03 21:19:18 -0400

    TEST COMMENT

--- valve_planner/src/valve_planner/drchubo_v2_wheel_turning.py
+++ valve_planner/src/valve_planner/drchubo_v2_wheel_turning.py
@@ -1021,6 +1021,7 @@ class DrcHuboV2WheelTurning( BaseWheelTurning ):
         cp.elements.append(cpe3)
         cp.elements.append(cpe4)
 
+        # TEST COMMENT
         # Plan for start -> goal -> start
         [success, why] = self.PlanPath(cp)
         if(not success):
